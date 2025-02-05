59415bc747aae8fdd1be051b86e8701b4c08ebcf
AlanDCV <alandavidc763@gmail.com>
2025-02-05 11:56:18 -0300

    Corrigiendo archivos ignorados

---
+++ probl6/problema6.cpp
@@ -0,0 +1,35 @@
+//Problema 6 del libro
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int n, ni, r;
+
+	cout << "Numero: "; cin >> n;
+
+	r = n % 10;
+	n = n / 10;
+	ni = r * 10;
+
+	r = n % 10;
+	n = n / 10;
+	ni = (ni + r) * 10;
+
+	r = n % 10;
+	n = n / 10;
+	ni = (ni + r) * 10;
+
+	r = n % 10;
+	n = n / 10;
+	ni = (ni + r) * 10;
+
+	r = n % 10;
+	n = n / 10;
+	ni = ni + r;
+
+	cout << "\nNumero inverso: " << ni;
+
+
+	return 0;
+}
